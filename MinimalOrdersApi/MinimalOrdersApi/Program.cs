d5192346d9c3f3ced348a0afc32a4962bbdb6e51
Vadym <mudretsvadim@gmail.com>
2023-06-19 23:53:05 +0300

    Connection to the database

--- MinimalOrdersApi/MinimalOrdersApi/Program.cs
+++ MinimalOrdersApi/MinimalOrdersApi/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalOrdersApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(opts =>
+        opts.UseSqlServer(builder.Configuration.GetConnectionString("BikeStores")));
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
@@ -10,5 +15,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("api/orders", (ApplicationDbContext dbContext) =>
+{
+    return dbContext.Products.First();
+});
 
 app.Run();
